41bfca3786cb635745701e5021b46fe7b45371ef
Carlos Alves <cp193116@gmail.com>
2022-11-10 22:14:05 -0300

    att0.0.0.0.0.1

--- login.php
+++ login.php
@@ -5,16 +5,19 @@
     $user=$_POST['f_nome'];
     $senha=$_POST['f_senha'];
 
-    $sql="select id from tb_usuarios where username='$user' and senha='$senha'";
+    $sql="select * from tb_usuarios where username='$user' and senha='$senha'";
     $res=mysqli_query($con, $sql);
     $linhas=mysqli_affected_rows($con);
 
     while($registros = mysqli_fetch_array($res)){
         $id = $registros['id'];
+        $nome = $registros['nome'];
+        $nomeusuario = $registros['username'];
+        $email = $registros['email'];
     }
 
     if($linhas > 0){
-        $_SESSION['msgteste'] = "<p> o ID do usuario é: $id";
+        $_SESSION['msgteste'] = "Bem vindo $nome seu id é: $id e seu email: $email";
         header("Location:paginaPrincipal.php");
         
     }else{
