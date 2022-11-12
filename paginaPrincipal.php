1585d4fb527abc3687f47129d1cf2388d72ef884
Carlos Alves <cp193116@gmail.com>
2022-11-12 18:19:50 -0300

    .

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="estiloPagPrincipal.css">
 </head>
 <body>
     <h1>Tudo ok</h1>
